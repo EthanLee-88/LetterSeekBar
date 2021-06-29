2526c6bc68263376fbbbbb4fd394f46b26242d01
EthanLee <809197443@qq.com>
2021-06-29 14:09:04 +0800

    新增seekBar动画效果

--- app/src/main/java/com/ethan/letterseekbar/ui/view/LettersSeekBar.java
+++ app/src/main/java/com/ethan/letterseekbar/ui/view/LettersSeekBar.java
@@ -1,5 +1,6 @@
 package com.ethan.letterseekbar.ui.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -16,7 +17,7 @@ import com.ethan.letterseekbar.R;
 import androidx.annotation.Nullable;
 
 /*******
- * 字母拖动条
+ * 字母索引表
  * created by Ethan Lee
  * on 2021/2/13
  *******/
@@ -43,6 +44,8 @@ public class LettersSeekBar extends View {
      */
     private float lettersTextSize = 12;
 
+    private float currentY = -1;
+
     private static final String[] lettersList = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
             "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
             "Y", "Z",};
@@ -50,7 +53,7 @@ public class LettersSeekBar extends View {
     /**
      * 当前选中的字母索引
      */
-    private int currentPosition;
+    private int currentPosition = -1;
 
     /**
      * 字母变化回调
@@ -87,7 +90,7 @@ public class LettersSeekBar extends View {
         highLinePaint.setColor(highLightColor);
         highLinePaint.setAntiAlias(true);
         highLinePaint.setDither(true);
-        highLinePaint.setTextSize(lettersTextSize);
+        highLinePaint.setTextSize(59f);
         typedArray.recycle();
     }
 
@@ -101,7 +104,7 @@ public class LettersSeekBar extends View {
         //控件的宽度取决于字体大小，字体大小+paddingLeft+paddingRight
         int textWidth = (int) normalPaint.measureText("A");//测量字体大小
         int width = textWidth + getPaddingLeft() + getPaddingRight();//计算出控件宽度
-        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec) + getPaddingTop() + getPaddingBottom();
         setMeasuredDimension(width, height);
     }
 
@@ -115,34 +118,53 @@ public class LettersSeekBar extends View {
         Paint.FontMetrics fontMetrics = normalPaint.getFontMetrics();
         int dy = (int) ((fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom);
 
-        for (int i = 0; i < lettersList.length; i++) {
-            int textWidth = (int) normalPaint.measureText(lettersList[i]);//测量每个字体大小
-            //计算出绘制字体X方向的其实位置，等于item宽度的一半减去字体宽度的一半，可实现字母居中
-            int x = itemWidth / 2 - textWidth / 2;
+        int x;
+        int textWidth = (int) normalPaint.measureText(lettersList[0]);//测量每个字体大小
+        //计算出绘制字体X方向的起始位置
+        x = itemWidth - getPaddingRight() - textWidth / 2;
 
+        for (int i = 0; i < lettersList.length; i++) {
             //计算出每个Item的Y方向上的中心
             int itemCenterY = itemHeight * i + itemHeight / 2 + getPaddingTop();
 
             //基线的位置等于 中线的位置 + dy
             int baseLine = itemCenterY + dy;
 
-            if (i == currentPosition) {  //选中，高亮
-                canvas.drawText(lettersList[i], x, baseLine, highLinePaint);
+            // 将与被选中的item相邻的几个item的 x位置向左移
+            if ((currentY > 0) && (Math.abs(currentY - itemCenterY) < 3 * itemHeight)) {
+                if (i == currentPosition) { //选中的item
+                    canvas.drawText(lettersList[i],
+                            (float) (x - (3.5 * itemHeight - Math.abs(currentY - itemCenterY))), baseLine, normalPaint);
+                    canvas.drawText(lettersList[i],
+                            (x - (6 * itemHeight - Math.abs(currentY - itemCenterY))), baseLine, highLinePaint);// 再画个大的
+                }else {
+                    canvas.drawText(lettersList[i], x - (3 * itemHeight - Math.abs(currentY - itemCenterY)), baseLine, normalPaint);
+                }
             } else {
                 canvas.drawText(lettersList[i], x, baseLine, normalPaint);
             }
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        // 缩小触控区域
+        if (event.getX() < (getWidth() - 2 * getPaddingRight() - normalPaint.measureText(lettersList[0]))) {
+            if (!((currentY == -1) && (currentPosition == -1))) {
+                currentY = -1;
+                currentPosition = -1;
+                invalidate();
+            }
+            return false;
+        }
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
                 //获取当前触点Y的值，再除以item的高度，得到触点的position
-                float y = event.getY();
+                currentY = event.getY();
                 int itemHeight = getHeight() / lettersList.length;
-                int newPosition = (int) (y / itemHeight);
+                int newPosition = (int) (currentY / itemHeight);
                 //防止数组越界
                 if (newPosition < 0) newPosition = 0;
                 if (newPosition > lettersList.length - 1) newPosition = lettersList.length - 1;
@@ -154,11 +176,19 @@ public class LettersSeekBar extends View {
                 }
                 invalidate();
                 break;
-                case MotionEvent.ACTION_UP:
-                    if (mLetterChangeListener != null) {
-                        mLetterChangeListener.letterChange(lettersList[currentPosition], true);
-                    }
-                    break;
+            case MotionEvent.ACTION_UP:
+                currentY = -1;
+                if (mLetterChangeListener != null) {
+                    mLetterChangeListener.letterChange(lettersList[currentPosition], true);
+                }
+                invalidate();
+                break;
+
+            default:
+                currentY = -1;
+                currentPosition = -1;
+                invalidate();
+                break;
         }
         return true;
 //        return super.onTouchEvent(event); // return true 表示拦截后续事件，如果是false则不会再拦截MOVE事件
